ab7d98780d2afc8a54cbd87278c8b0e16fb47025
io-Jacob <jcy.youngblood@gmail.com>
2024-09-13 13:44:17 -0500

    testing put data in data property

--- PR62-1I.js
+++ PR62-1I.js
@@ -5,11 +5,13 @@ function decodeUplink(input) {
 	// Check for MODE message
 	if(bytes[0] == 0x7a){
 		return {
-			text: "Mode Message Received",
-			deviceType: bytes[1],
-			// mode: Buffer.from(bytes.slice(2,5)).toString(),
-			mode: String.fromCharCode(bytes[2])+String.fromCharCode(bytes[3])+String.fromCharCode(bytes[4]),
-			packet: bytes
+			data: {
+				text: "Mode Message Received",
+				deviceType: bytes[1],
+				// mode: Buffer.from(bytes.slice(2,5)).toString(),
+				mode: String.fromCharCode(bytes[2])+String.fromCharCode(bytes[3])+String.fromCharCode(bytes[4]),
+				packet: bytes
+			}
 		}
 	}
 	// Check for command response message
@@ -22,18 +24,22 @@ function decodeUplink(input) {
 			responses = responses.slice(3+responses[2]);
 		}while(responses.length);
 		return {
-			text: "Command Response Received",
-			responses: report,
-			packet: bytes
+			data:{
+				text: "Command Response Received",
+				responses: report,
+				packet: bytes
+			}
 		}
 	}
 	// Finally check for erroneous data
 	if(bytes.length != dataPacketLength){
 		return {
-			errors: ["Invalid Sensor Data sent to parser. Expected Payload with length of "+dataPacketLength],
-			packet: bytes,
-			expectedLength: dataPacketLength,
-			receivedLength: bytes.length
+			data:{
+				errors: ["Invalid Sensor Data sent to parser. Expected Payload with length of "+dataPacketLength],
+				packet: bytes,
+				expectedLength: dataPacketLength,
+				receivedLength: bytes.length
+			}
 		}
 	}
 	// Decode each part of the payload
